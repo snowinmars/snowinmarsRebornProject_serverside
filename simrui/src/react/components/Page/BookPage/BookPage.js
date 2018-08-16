b1f9c5f182a98a9a419530fd6e12da7602555ce8
snowinmars <stuff4work@yandex.ru>
2018-08-16 22:34:19 +0300

    Loading state for the book table is now correct

--- simrui/src/react/components/Page/BookPage/BookPage.js
+++ simrui/src/react/components/Page/BookPage/BookPage.js
@@ -11,9 +11,38 @@ class BookPage extends Component {
     constructor(props) {
         super(props);
 
+        var defaultUser = {
+            title: 'Loading...',
+            authors: [
+                {
+                    name: {
+                        givenName: 'Loading...',
+                        fullMiddleName: 'Loading...',
+                        familyName: 'Loading...',
+                        key: 0
+                    }
+                }
+            ],
+            year: 'Loading...',
+            imageUrl: 'Loading...',
+            additionalInfo: 'Loading...',
+            bookshelf: 'Loading...',
+            flibustaUrl: 'Loading...',
+            libRusEcUrl: 'Loading...',
+            liveLibUrl: 'Loading...',
+            owner: 'Loading...',
+            pageCount: 'Loading...',
+            key: 0
+        };
+
         var data = [];
 
+        for (var i = 0; i < 10; i++) {
+            data.push(defaultUser);
+        }
+
         this.state = {
+            isInit: false,
             response: {
                 code: -1,
                 data: data
@@ -54,33 +83,40 @@ class BookPage extends Component {
     render() {
         const options = Config.bootstrapTableOptions;
 
-        return (
-            <BootstrapTable
-                ref="table"
-                data={this.state.response.data}
-                striped
-                hover
-                pagination
-                expandableRow={this.isExpandableRow}
-                expandComponent={this.expandComponent}
-                options={options}
+        const table = <BootstrapTable
+            ref="table"
+            data={this.state.response.data}
+            striped
+            hover
+            pagination
+            expandableRow={this.isExpandableRow}
+            expandComponent={this.expandComponent}
+            options={options}
+        >
+            <TableHeaderColumn dataField="title" isKey dataSort>
+                Title
+                </TableHeaderColumn>
+
+            <TableHeaderColumn
+                dataField="authors"
+                dataSort
+                dataFormat={this.authorsFormatter}
             >
-                <TableHeaderColumn dataField="title" isKey dataSort>
-                    Title
+                Authors
                 </TableHeaderColumn>
 
-                <TableHeaderColumn
-                    dataField="authors"
-                    dataSort
-                    dataFormat={this.authorsFormatter}
-                >
-                    Authors
+            <TableHeaderColumn dataField="year" dataSort>
+                Year
                 </TableHeaderColumn>
+        </BootstrapTable>
 
-                <TableHeaderColumn dataField="year" dataSort>
-                    Year
-                </TableHeaderColumn>
-            </BootstrapTable>
+        const loader = !this.state.inInit && <div className="simr-loader"></div>
+
+        return (
+            <div>
+                {loader}
+                {table}
+            </div>
         );
     }
 }
